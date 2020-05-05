d5f6bee90f7a5537e226a26c914292e06e36341c
Wolfram Diestel <diestel@steloj.de>
2020-05-05 07:24:08 +0200

    elŝutu ĉiujn partojn (hst, art, html)

--- bin/revo_download_gh.sh
+++ bin/revo_download_gh.sh
@@ -2,13 +2,36 @@
 #!/bin/bash
 #set -e
 api=https://api.github.com
-url=$(curl -s "${api}/repos/revuloj/revo-fonto/releases/latest" | \
-    jq -r '.assets[] | select(.name | startswith("revohtml_") ) | .url' )
+eld=$( curl -s "${api}/repos/revuloj/revo-fonto/releases/latest" | jq -c '.assets[]' )
+
+html_url=$( echo $eld | jq -r 'select(.name|startswith("revohtml_")) | .url' )
+art_url=$(  echo $eld | jq -r 'select(.name|startswith("revoart_"))  | .url' )
+hst_url=$(  echo $eld | jq -r 'select(.name|startswith("revohst_"))  | .url' )
     
-if [[ ! -z "$url" ]]; then    
-    echo "downloading ${url}"
-    echo "to: revohtml.zip"
-    curl -L -H "Accept: application/octet-stream" -o revohtml.zip "${url}"
-    unzip -qo revohtml.zip
-    rm revohtml.zip
-fi    
+if [[ ! -z "$html_url" ]]; then    
+    echo "revohtml.zip <- ${html_url}"
+    curl -L -H "Accept: application/octet-stream" -o revohtml.zip "${html_url}"
+else
+    echo "ERARO: Arĥivo revohtml_*.zip ne troviĝis en la lasta eldono!"
+    exit 1
+fi
+
+if [[ ! -z "$art_url" ]]; then    
+    echo "revoart.zip <- ${art_url}"
+    curl -L -H "Accept: application/octet-stream" -o revoart.zip "${art_url}"
+else
+    echo "ERARO: Arĥivo revoart_*.zip ne troviĝis en la lasta eldono!"
+    exit 1
+fi
+
+if [[ ! -z "$hst_url" ]]; then    
+    echo "revohst.zip <- ${html_url}"
+    curl -L -H "Accept: application/octet-stream" -o revohst.zip "${hst_url}"
+else
+    echo "ERARO: Arĥivo revohst_*.zip ne troviĝis en la lasta eldono!"
+    exit 1
+fi
+
+unzip -qo revohtml.zip && rm revohtml.zip
+unzip -qo revoart.zip && rm revoart.zip
+unzip -qo revohst.zip && rm revohst.zip
