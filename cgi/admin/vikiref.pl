a0b1c39a0409b808767363a8af2ec708961d7f50
Wolfram Diestel <diestel@steloj.de>
2021-03-27 22:02:06 +0100

    aldonado de mankanta viki-referenco en la kapvorto funkcias, sed la rezulta HTML-dokumento ŝajnas nevalida (pro />?)

--- cgi/admin/vikiref.pl
+++ cgi/admin/vikiref.pl
@@ -30,14 +30,20 @@ use utf8; binmode STDOUT, ":utf8";
 my $homedir = "/hp/af/ag/ri";
 #my $vikiref = "$homedir/www/revo/inx/vikiref.xml"
 my $vikiref = "./tmp/vikiref.xml";
+# PLIBONIGU: okaze ŝanĝu al https!
+my $vpref = 'http://eo.wikipedia.org/wiki';
 
-$debug = 1;
-$verbose = 1;
+my $debug = 1;
+my $verbose = 1;
+
+my $modified;
+my $doc; # la aktuala artikola DOM, globale ni ŝparas sub-argumenton plurloke...
+
 @artikoloj = @ARGV;
 
 # enlegu la antaŭpreparitan referencliston
 my %refs; my $rcnt = load_vikiref();
-die "$vikiref enhavas tro malmultajn elementojn\n" unless($rcnt>10000);
+die "$vikiref enhavas tro malmultajn elementojn!\n" unless($rcnt>10000);
 
 # nun ni havas la indeksitan liston de ĉiuj referencoj kaj 
 # trairas ĉiujn artikolojn por aldoni ankoraŭ mankantajn referencojn
@@ -66,41 +72,125 @@ sub load_vikiref {
         $refs->{$file}->{$mrk} = $a->getNamedItem('v')->textContent();
         $count++;        
     }
-    print "vikiref: $count referencoj\n" if ($verbose);
+    print "### vikiref: $count referencoj ###\n" if ($verbose);
     return $count;
 }    
 
 sub process_art {
     my $artikolo = shift;
+   
     my $file = (split /\./, basename($artikolo))[0];
     my $arefs = $refs->{$file};
 
+
+    # TODO: provizore ni nur traktas la dosieron, se ni havas referencojn
+    # poste ni devos ankaŭ forigi referencojn, kiuj ne plu ekzistas!
     return unless($arefs);    
 
     # ni reskribas ĉion al la sama artikolo, kiam ni
     # uzas git-versiadon!
+    $modified = 0;
     my $artout = $artikolo; #.".out";
-    my $modified = 0;
 
-    print "### ",uc($artikolo)," ###\n" if ($verbose);
+    print "### ",$artikolo," ###\n" if ($verbose);
 
     # load HTML
     # vd ankaŭ https://metacpan.org/pod/distribution/XML-LibXML/lib/XML/LibXML/Parser.pod#Parser-Options
     $doc = XML::LibXML->load_html(location => $artikolo, validation=>0, recover=>2, expand_entities=>0); #, keep_blanks=>1);
 
-
-    print "DOM!\n";
-
     # trovu art@mrk kaj altigu la version...
     #my $kapoj = $doc->findnodes('//section[@class="drv"]/h2');
     my $kapoj = $doc->findnodes('//section[@id="s_artikolo"]//h2');
 
-    for $k (@$kapoj) {      
-        # kolektu nur rektan tekstenhavon kaj ignoru subelementojn  
-        my $text; 
-        for $c ($k->childNodes) { $text .= $c->nodeValue(); } 
-        # montru
-        print "KAP: $text\n$k\n" if ($debug);
+    for $K (@$kapoj) { 
+        kap_update($K,$arefs);
     }
 
+    # nur skribu, se ni efektive aldonis tradukojn, ĉar
+    # ankaŭ ŝanĝiĝas iom linirompado kaj kodado de unikodaj literoj en la XML
+    if ($modified) {
+        print "### skribas aktualigitan artikolon $artout...\n";
+        open OUT, ">", $artout || die "Ne povas skribi al '$artout': $!\n";        
+        print OUT $doc;
+        close OUT;
+    } else {
+        print "  # ne ŝanĝita\n" if ($verbose);
+    }
 }
+
+sub kap_update {
+    my ($K,$arefs) = @_;
+    my $vref;
+
+    # kolektu nur rektan tekstenhavon kaj ignoru subelementojn  
+    my $kap; 
+    for $ch ($K->childNodes) { 
+        if ($ch->nodeType eq XML_TEXT_NODE || $ch->nodeType eq XML_ENTITY_REF_NODE) {
+            $kap .= $ch->nodeValue(); 
+        } elsif ( $ch->nodeType eq XML_ELEMENT_NODE 
+          && $ch->nodeName() eq 'a'
+          && $ch->hasAttribute('alt') ) {                
+            # rigardu, ĉu tio estas Vikipedio-referenco kaj se jes elprenu href
+            my $a = $ch->attributes();
+            if ( $a->getNamedItem('alt')->textContent() eq 'Vikipedio') {
+                $vref = $a->getNamedItem('href')->textContent();
+            }
+        }
+    }
+    $kap = trim($kap);
+
+    # montru
+    print "KAP: /$kap/\n[$K]\n" if ($debug);
+
+    # PLIBONIGU: referencoj (markoj) montras foje al nura dosiero
+    # tiam verŝajne ni rilatigu ilin al la unua derivaĵo de la artikolo...
+    # ĉu ni ŝanĝu tie al konkreta derivaĵo aŭ ĉu ni aŭtomate ŝovu en la unuan de la artikolo?
+    my $id = $K->attributes()->getNamedItem('id')->textContent();
+    print "id: $id\n" if ($debug);
+    my $v = $arefs->{$id};
+    if ($v) { 
+        print "viki: $v\n" if ($debug); 
+
+        # se enestas $vref kaj tiu diferencas, ni plendu, en pli
+        # posta prilaboro ni povus aktualigi, sed ni unue volas kontroli kiel
+        # la nova skripto laboras...
+        if ($vref && $vref ne "$vpref/$v") {
+            print "malsama referenco: $vref != $v ($id)\n"
+        } elsif (! $vref) {
+            # enmetu novan Viki-referencon
+            kreu_vref($K,$v);
+            $modified = 1;
+        }
+    }
+}
+
+sub kreu_vref {
+    my ($K,$vref) = @_;
+
+    # PLIBONIGU: uzante SVG-fonon ni iam povos rezigni pri la bildeto, sed por ne konfuzi 
+    # la malnovan artikolprezenton ni porvizore restas ĉe tio...
+    my $img = make_el('img',(
+        alt=>'Vikipedio', 
+        src=>'../smb/vikio.png',
+        title=>'Al Vikipedio',
+        border=>'0'));
+    my $a = make_el('a',(
+        href=>"$vpref/$vref",
+        target=>'_new'));
+    $a->appendChild($img);
+    $K->appendChild($a);
+}
+
+
+# kreu novan elementon inkl. de atributoj
+sub make_el{
+    my ($name,%attr) = @_;
+    my $el = $doc->createElement($name);    
+    while (($key, $val) = each %attr) {
+        $el->setAttribute( $key, $val);
+    }
+    return $el;
+}
+
+# forigu spacojn komence kaj fine de signoĉeno
+sub trim { my $s = shift; $s =~ s/^\s+|\s+$//g; return $s };
