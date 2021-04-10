51ea5d50c0bdf38dac9c1b1f5762f9cf94a9493d
Wolfram Diestel <diestel@steloj.de>
2021-04-10 17:35:46 +0200

    uzo de v3tezauro por tez-referencoj

--- cgi/vokoref-json.pl
+++ cgi/vokoref-json.pl
@@ -52,53 +52,29 @@ sub viki_refs {
 }
 
 sub tez_refs {
-    # ni kolektas referencojn en ambaŭ referencoj, por la kontraŭa direkto 
-    # ni interŝanĝas fonton kaj celon kaj la referenctipon:
-    # dif->sin, sub->super, super->sub, prt->malprt, malprt->prt, ekz->super
-    # KOREKTU: ni devus aldoni DISTINCT sur la plej supra nivelo, sed
-    # prefere kreu UNION kiel VIEW kaj poste ni povas tie elekti neduoblajn...
-    my $rows = $dbh->selectall_arrayref( #"SELECT DISTINCT * FROM (" - duoblaĵoj venas precipe pro diversaj markoj drv / snc[0]
-          "SELECT tez_fontteksto AS fk, tez_fontref AS fm, tez_fontn AS fn, "
-        .   "tez_celteksto AS ck, tez_celref AS cm, tez_celn AS cn, "
-        .   "tez_tipo AS tip, tez_fako AS fak FROM r2_tezauro "
-        . "WHERE tez_kapvorto = '$art' AND tez_celteksto != '???' "
-        .   "AND tez_fontref IS NOT NULL AND tez_celref IS NOT NULL "
-        . "UNION SELECT tez_celteksto AS fk, tez_celref AS fn, tez_celn AS fn, "
-        .   "tez_fontteksto AS ck, tez_fontref AS cm, tez_fontn AS cn, "
-        .   "CASE tez_tipo WHEN 'dif' THEN 'sin' WHEN 'sub' THEN 'sup' WHEN 'sup' THEN 'sub' "
-        .     "WHEN 'prt' THEN 'malprt' WHEN 'mal' THEN 'prt' WHEN 'ekz' THEN 'super' "
-        .     "ELSE tez_tipo END AS tip, tez_fako AS fak FROM r2_tezauro "
-        . "WHERE tez_celref LIKE '$art.%' AND tez_celteksto != '???' "
-        .   "AND tez_fontref IS NOT NULL " #  . ") AS referencoj 
+    my $rows = $dbh->selectall_arrayref("SELECT DISTINCT mrk,tip,cel,lst,kap,num "
+        . "FROM v3tezauro WHERE mrk LIKE '$art.%' "
         . "LIMIT $LIMIT", { Slice=>{} }); 
-        # fakte referencoj sen mrk (tez_fontref, tez_celref) ne havas sencon, sed ili enestas tamen,
-        # ekz-e por marki fakon de artikolo - eble iam plibonirug parseart2 kaj la rtabelon r2_tezauro
-        # fakindikoj povus havi lokon en alia tabelo!
+    
+
     my @refs;
     # strukturu la rezultojn
     for my $row (@$rows) {        
-        my $fnt = { 
-            'm' => $row->{fm},
-            'k' => $row->{fk}
-        };
-        $fnt->{n} = $row->{fn} if ($row->{fn});
-
         my $cel = { 
-            'm' => $row->{cm},
-            'k' => $row->{ck}
+            'm' => $row->{cel},
+            'k' => $row->{kap}
         };
-        $cel ->{n} = $row->{cn} if ($row->{cn});
+        $cel ->{n} = $row->{num} if ($row->{num});
+        $cel ->{l} = $row->{lst} if ($row->{lst});
 
         my $ref = {
-            'tip'=> ($row->{tip} eq 'sup'? 'super' : $row->{tip}),
-            'fnt'=> $fnt,
+            'tip'=> $row->{tip},
+            'mrk'=> $row->{mrk},
             'cel'=> $cel
         };
-        $ref ->{fak} = $row->{fak} if ($row->{fak});
+        #$ref ->{fak} = $row->{fak} if ($row->{fak});
         push @refs, $ref;
     }
     # redonu la rezulton
     return \@refs;
 }
-
-	
