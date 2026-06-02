d15a469fb95d7af5237f516434d98c88547b42f1
Wolfram Diestel <diestel@steloj.de>
2026-06-02 22:07:11 +0200

    kor.

--- tst/cgi/22_adm_credentials.sh
+++ tst/cgi/22_adm_credentials.sh
@@ -4,7 +4,7 @@ stack=araneujotesto
 araneo_id=$(docker ps --filter name=${stack}_araneo -q) 
 
 cgi_user=araneo
-cgi_pwd=$(docker exec ${araneo_id} cat /run/secrets/voko-araneo.cgi_password | head -n 1)
+cgi_pwd=$(docker exec ${araneo_id} cat /run/secrets/voko-araneo.cgi_password)
 
 b64=$(echo -n "${cgi_user}:${cgi_pwd}" | base64)
 echo -n "$b64"
